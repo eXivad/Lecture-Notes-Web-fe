57609b936134d3e2c2892a38c8b728057b759ae7
Davide Tarulli <davidetarulli.dev@gmail.com>
2024-10-25 15:26:12 +0200

    Refactor page component and add DropBoxComponent

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { DropBoxComponent } from "@/components/lecture-notes-converter";
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-primary">Lecture Notes Web</h1>
-      <p>Converti le tue slide in pratici blocchi appunti</p>
+    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col gap-1 items-center">
+        <h1 className="text-4xl font-bold text-primary">Lecture Notes Web</h1>
+        <p>Converti le tue slide in pratici blocchi appunti</p>
+      </div>
 
       <DropBoxComponent />
       
