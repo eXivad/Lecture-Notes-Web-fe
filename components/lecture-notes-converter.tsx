5bbaa666b953785200c25343dd179ad9f78d99cb
Davide Tarulli <davidetarulli.dev@gmail.com>
2024-11-05 17:52:00 +0100

    Refactor DropBoxComponent, add Badge component, handle file conversion, and update styles

--- components/lecture-notes-converter.tsx
+++ components/lecture-notes-converter.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
-import { Trash2, Upload, LoaderCircle } from 'lucide-react'
+import { Trash2, Upload, LoaderCircle, Download, RotateCcw} from 'lucide-react'
 
 import { Button } from "@/components/ui/button"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
@@ -20,11 +20,13 @@ type FileWithOptions = {
   paperType: PaperType;
   isConverting?: boolean
   errorConverting?: boolean
+  downloaded?: boolean
 }
 
 export function DropBoxComponent() {
   const [files, setFiles] = useState<FileWithOptions[]>([])
   const [isConverting, setIsConverting] = useState<boolean>(false)
+  const [isConverted, setIsConverted] = useState<boolean>(false)
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     setFiles(prevFiles => [
@@ -52,35 +54,84 @@ export function DropBoxComponent() {
     setFiles(prevFiles => prevFiles.filter((_, index) => index !== fileIndex))
   }
 
-  const convertFile = async (file: FileWithOptions) => {
+  const handleDownloadFile = async (fileIndex: number) => {
+    setFiles(prevFiles => 
+      prevFiles.map((file, index) => 
+        index === fileIndex ? { ...file, downloaded: true} : file
+      )
+    )
+
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_CONVERTER_SITE!+files[fileIndex].fileConverted)
+      if (!response.ok) {
+        throw new Error('Errore durante il download del file')
+      }
+
+      const blob = await response.blob()
+      const url = window.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = files[fileIndex].file.name
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+      window.URL.revokeObjectURL(url)
+    } catch (error) {
+      console.error('Errore durante il download del file:', error)
+    }
+
+    
+  }
+
+  const convertFile = async (fileToConvert: FileWithOptions) => {
     const formData = new FormData()
-    formData.append('pdftoconvert', file.file)
-    formData.append('paper_type', file.paperType)
+    formData.append('pdftoconvert', fileToConvert.file)
+    formData.append('paper_type', fileToConvert.paperType)
 
-    file.isConverting = true
-    /*
-    const response: Response = await fetch(process.env.NEXT_PUBLIC_API_CONVERTER_SITE!, {
-      method: 'POST',
-      mode: 'cors',
-      body: formData
-    })
+    //Aggiorna il file in Conversione
+    setFiles(prevFiles => 
+      prevFiles.map(file => 
+        file.file === fileToConvert.file ? { ...file, isConverting: true, errorConverting: false } : file
+      )
+    )
 
-    if (!response.ok){
-      file.errorConverting = true
-      throw new Error('Errore durante la conversione del file')
+    try{
+      const response: Response = await fetch(process.env.NEXT_PUBLIC_API_CONVERTER_SITE!+"/generate", {
+        method: 'POST',
+        mode: 'cors',
+        body: formData
+      })
+
+      if (!response.ok) 
+        throw new Error()
+
+      const fileConvertedData = await response.json()
+      
+      if (!fileConvertedData) 
+        throw new Error()
+
+      // Aggiorna lo stato per indicare che la conversione è completata
+      setFiles(prevFiles => 
+        prevFiles.map(file => 
+          file.file === fileToConvert.file ? { ...file, isConverting: false, fileConverted: fileConvertedData.download_link, errorConverting: false } : file
+        )
+      )
+      
+    }catch(e){
+      setFiles(prevFiles => 
+        prevFiles.map(file => 
+          file.file === fileToConvert.file ? { ...file, isConverting: false, errorConverting: true } : file
+        )
+      )
+
+      throw e
+    } finally {
+      setFiles(prevFiles => 
+        prevFiles.map(file => 
+          file.file === fileToConvert.file ? { ...file, isConverting: false } : file
+        )
+      )
     }
-
-    const fileConvertedData = await response.json()
-
-    if (!fileConvertedData){
-      file.errorConverting = true
-      throw new Error('Errore durante la conversione del file')
-    }
-
-    file.isConverting = false
-    file.fileConverted = fileConvertedData.download_link
-    */
-    // TODO: Devi provare a capire se i badge funzionano facendo attenzione a cambiare le opzioni con setFiles
   }
 
   const handleSubmit = async () => {
@@ -93,12 +144,13 @@ export function DropBoxComponent() {
       console.error('Errore durante la conversione dei file:', error)
     } finally {
       setIsConverting(false)
+      setIsConverted(true)
     }
   }
 
   return (
     <div>
-      <div {...getRootProps()} className={`-full border-2 border-dashed rounded-lg p-8 mb-6 text-center cursor-pointer transition-colors ${isDragActive ? 'border-primary bg-primary/10' : 'border-gray-300 hover:border-primary'}`}>
+      <div {...getRootProps()} className={`w-full border-2 border-dashed rounded-lg p-8 mb-6 text-center cursor-pointer transition-colors ${isDragActive ? 'border-primary bg-primary/10' : 'border-gray-300 hover:border-primary'} ${isConverting || isConverted ? "hidden" : ""}`}>
         <input {...getInputProps()} />
         <Upload className="mx-auto mb-2" size={24} />
         <p>{isDragActive ? "Rilascia i file qui" : "Trascina i file PDF qui, o clicca per selezionarli"}</p>
@@ -122,27 +174,43 @@ export function DropBoxComponent() {
                     ))}
                   </SelectContent>
                 </Select>
-                <Badge className="rounded-full" variant={file.errorConverting ? "destructive" : "secondary"}>
-                  {file.isConverting ? <>Convertendo <LoaderCircle className='animate-spin'/></> : <>Aggiunto</>}
-                  {file.errorConverting ? <>Errore</> : <></>}
+                <Badge className="rounded-full flex gap-2" variant={file.fileConverted ? "success" : file.errorConverting ? "destructive" : file.isConverting ? "secondary" : "outline"}>
+                  {file.fileConverted ? <>Terminato</> : null}
+                  {file.isConverting ? <>Convertendo <LoaderCircle size={10} className='animate-spin'/></> : null}
+                  {file.errorConverting ? <>Errore</> : null}
+                  {!file.isConverting && !file.errorConverting && !file.fileConverted ? <>Aggiunto</> : null}
                 </Badge>
-                <Button variant="destructive" size="icon" onClick={() => handleRemoveFile(index)} className="">
-                  <Trash2 size={18} />
-                </Button>
+                {!file.fileConverted ? 
+                  <Button variant="destructive" size="icon" onClick={() => handleRemoveFile(index)} className="">
+                    <Trash2 size={18} />
+                  </Button>
+                  :
+                  <Button disabled={file.downloaded} onClick={() => handleDownloadFile(index)}>
+                    <Download size={18}/>
+                    {!file.downloaded ? "Scarica" : "Scaricato"}
+                  </Button>                
+                }
+                
               </li>
             ))}
           </ul>
         </div>
       )}
 
-      <Button onClick={handleSubmit} disabled={files.length === 0 || isConverting} className="w-full">
+      <Button onClick={!isConverted ? handleSubmit : () => window.location.reload()} disabled={files.length === 0 || isConverting} className="w-full">
         {isConverting ? 
         <>
-          Convertendo
           <LoaderCircle size={18} className='animate-spin'/>
+          Convertendo
         </>
         :
-        <>Converti File</>
+        isConverted ?
+        <>
+        <RotateCcw size={18}/>
+        Converti Anccora
+        </>
+        :
+        <>Converti</>
         }
       </Button>
     </div>
